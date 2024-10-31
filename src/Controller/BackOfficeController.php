69a054d43e28015191984e40c8788c2af6a6cfe8
Anokino <nkoloiz58@gmail.com>
2024-10-31 01:38:03 +0100

    ajout des questions au backoffice

--- src/Controller/BackOfficeController.php
+++ src/Controller/BackOfficeController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\MotDuJour;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Questions;
 
 class BackOfficeController extends AbstractController
 {
@@ -16,7 +17,8 @@ class BackOfficeController extends AbstractController
     {
         return $this->render('back_office/index.html.twig', [
             'controller_name' => 'BackOfficeController',
-            'mot_du_jour' => $entityManager->getRepository(MotDuJour::class)->findOneBy([], ['id' => 'DESC'])
+            'mot_du_jour' => $entityManager->getRepository(MotDuJour::class)->findOneBy([], ['id' => 'DESC']),
+            'questions' => $entityManager->getRepository(Questions::class)->findAll()
             
         ]);
     }
@@ -41,6 +43,23 @@ class BackOfficeController extends AbstractController
         $entityManager->persist($motDuJour);
         $entityManager->flush();
         
+        return $this->redirectToRoute('app_back_office');
+    }
+    #[Route('/backoffice/modif_question/{id}', name: 'app_back_office_modif_question', methods: ['POST'])]
+    public function modifQuestion(Request $request, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $question = $entityManager->getRepository(Questions::class)->find($id);
+        $question->setQuestion($request->request->get('question'));
+        $entityManager->persist($question);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_back_office');
+    }
+    #[Route('/backoffice/delete_question/{id}', name: 'app_back_office_delete_question', methods: ['POST'])]
+    public function deleteQuestion(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $question = $entityManager->getRepository(Questions::class)->find($id);
+        $entityManager->remove($question);
+        $entityManager->flush();
         return $this->redirectToRoute('app_back_office');
     }
 }
