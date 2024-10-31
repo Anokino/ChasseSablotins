3ac13df981d0eb029e639b55fe7210348d10acc8
Anokino <nikolas.loizeau@gmail.com>
2024-10-31 16:32:41 +0100

    style css qrcode reader

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,7 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+// Le style du scanner QR code est importé directement dans les templates qui en ont besoin
+
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
